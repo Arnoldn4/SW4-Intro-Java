602b420f47a50a039aac22396ab669928428bfc6
ArnoldNyakunengwa <arnoldn4yahoo.co.uk>
2023-01-22 18:17:08 +0000

    initial version

---
+++ src/SquareCalculatorExt.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class SquareCalculatorExt {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        // assign value width: 13.5 and length: 25.3
+        // data type is double to accept decimal number
+
+        System.out.println("Enter width value : ");
+        double width = reader.nextDouble();
+        System.out.println("Enter length value : ");
+        double length = reader.nextDouble();
+        System.out.println("The width of rectangle is " + width);
+        System.out.println("The length of rectangle is " + length);
+        double area = width * length;
+        System.out.println("The area of the rectangle is " + area);
+
+    }
+}
+
