270f1a399f1afafd24a969e6a67a27df294313f7
Arnoldn4 <122799842+Arnoldn4@users.noreply.github.com>
2023-09-09 12:40:49 +0100

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld { // main class
     public static void main(String[] args) { // 'main' shortcut
     // START HERE
-        System.out.println("Hello, this is my first program in INTELLIJ IDEA");
+        System.out.println("Hello, this is my first program in Java using INTELLIJ IDEA");
         // sout shortcut
         System.out.println("Printing a tree");
         System.out.println("        *        ");
